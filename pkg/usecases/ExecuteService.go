7dfda67d3f0d33ff11739045498d2d05489e4344
exartt <lmoraes1644cadastros@gmail.com>
2023-10-07 15:45:04 -0300

    up

--- pkg/usecases/ExecuteService.go
+++ pkg/usecases/ExecuteService.go
@@ -102,7 +102,7 @@ func (es *ExecuteService) Execute() entities.ExecutionResult {
 	}
 }
 
-var ioSemaphore = make(chan struct{}, 10)
+var ioSemaphore = make(chan struct{}, 15)
 
 func (es *ExecuteService) processFile(wg *sync.WaitGroup, tempFile string,
 	processedFiles chan string,
